77cbf488b73eb801c9ee8588364749492d24a04b
Unitech <alexandre@keymetrics.io>
2021-11-22 11:02:51 +0100

    example sensor

---
+++ README.md
@@ -0,0 +1,9 @@
+## Heltec AB01 + LowCostLoraGW
+
+Working example with right pins definition for Heltec HTCC-AB01 to use with forked StuartsProjects/SX12XX-LoRa [LowCostLoraGW](https://github.com/CongducPham/LowCostLoRaGw/tree/master/Arduino/libraries/SX12XX)
+
+To run this example you will need platform.io installed and run this command:
+
+```
+pio run && pio run --target upload && pio device monitor -b 115200
+```
---
+++ src/Arduino_LoRa_SX12XX_Demo_Sensor.ino
@@ -0,0 +1,542 @@
+/*
+ *  Demo of a simple temperature sensor on analog A0 to test the LoRa gateway
+ *
+ *  Copyright (C) 2016-2020 Congduc Pham, University of Pau, France
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************
+ * last update: November 23rd, 2020 by C. Pham
+ *
+ * NEW: LoRa communicain library moved from Libelium's lib to StuartProject's lib
+ * https://github.com/StuartsProjects/SX12XX-LoRa
+ * to support SX126X, SX127X and SX128X chips (SX128X is LoRa in 2.4GHz band)
+ *
+ */
+
+#include <SPI.h>
+//this is the standard behaviour of library, use SPI Transaction switching
+#define USE_SPI_TRANSACTION
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// please uncomment only 1 choice
+#define SX126X
+//#define SX127X
+//#define SX128X
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef SX126X
+#include <SX126XLT.h>
+#include "SX126X_RadioSettings.h"
+#endif
+
+#ifdef SX127X
+#include <SX127XLT.h>
+#include "SX127X_RadioSettings.h"
+#endif
+
+#ifdef SX128X
+#include <SX128XLT.h>
+#include "SX128X_RadioSettings.h"
+#endif
+
+#include "my_demo_sensor_code.h"
+
+// http://patorjk.com/software/taag
+/********************************************************************
+ _____              __ _                       _   _
+/  __ \            / _(_)                     | | (_)
+| /  \/ ___  _ __ | |_ _  __ _ _   _ _ __ __ _| |_ _  ___  _ __
+| |    / _ \| '_ \|  _| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \
+| \__/\ (_) | | | | | | | (_| | |_| | | | (_| | |_| | (_) | | | |
+ \____/\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
+                          __/ |
+                         |___/
+********************************************************************/
+
+#if defined ARDUINO_ESP8266_ESP01 || defined ARDUINO_ESP8266_NODEMCU || defined ESP8266
+//uncomment if you want to disable WiFi on ESP8266 boards
+//#include <ESP8266WiFi.h>
+#endif
+
+///////////////////////////////////////////////////////////////////
+// COMMENT OR UNCOMMENT TO CHANGE FEATURES.
+// ONLY IF YOU KNOW WHAT YOU ARE DOING!!! OTHERWISE LEAVE AS IT IS
+#define OLED
+#define OLED_9GND876
+//uncomment to use a customized frequency.
+//#define MY_FREQUENCY 868100000
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+// CHANGE HERE THE NODE ADDRESS
+uint8_t node_addr=9;
+//////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+// IF YOU SEND A LONG STRING, INCREASE THE SIZE OF MESSAGE
+uint8_t message[50];
+///////////////////////////////////////////////////////////////////
+
+//create a library class instance called LT
+//to handle LoRa radio communications
+
+#ifdef SX126X
+SX126XLT LT;
+#endif
+
+#ifdef SX127X
+SX127XLT LT;
+#endif
+
+#ifdef SX128X
+SX128XLT LT;
+#endif
+
+//keep track of the number of successful transmissions
+uint32_t TXPacketCount=0;
+
+/*****************************
+ _____           _
+/  __ \         | |
+| /  \/ ___   __| | ___
+| |    / _ \ / _` |/ _ \
+| \__/\ (_) | (_| |  __/
+ \____/\___/ \__,_|\___|
+*****************************/
+
+// we wrapped Serial.println to support the Arduino Zero or M0
+#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0
+#define PRINTLN                   SerialUSB.println("")
+#define PRINT_CSTSTR(param)       SerialUSB.print(F(param))
+#define PRINTLN_CSTSTR(param)     SerialUSB.println(F(param))
+#define PRINT_STR(fmt,param)      SerialUSB.print(param)
+#define PRINTLN_STR(fmt,param)    SerialUSB.println(param)
+#define PRINT_VALUE(fmt,param)    SerialUSB.print(param)
+#define PRINTLN_VALUE(fmt,param)  SerialUSB.println(param)
+#define PRINT_HEX(fmt,param)      SerialUSB.print(param,HEX)
+#define PRINTLN_HEX(fmt,param)    SerialUSB.println(param,HEX)
+#define FLUSHOUTPUT               SerialUSB.flush()
+#else
+#define PRINTLN                   Serial.println("")
+#define PRINT_CSTSTR(param)       Serial.print(F(param))
+#define PRINTLN_CSTSTR(param)     Serial.println(F(param))
+#define PRINT_STR(fmt,param)      Serial.print(param)
+#define PRINTLN_STR(fmt,param)    Serial.println(param)
+#define PRINT_VALUE(fmt,param)    Serial.print(param)
+#define PRINTLN_VALUE(fmt,param)  Serial.println(param)
+#define PRINT_HEX(fmt,param)      Serial.print(param,HEX)
+#define PRINTLN_HEX(fmt,param)    Serial.println(param,HEX)
+#define FLUSHOUTPUT               Serial.flush()
+#endif
+
+#ifdef OLED
+#include <U8x8lib.h>
+//you can also power the OLED screen with a digital pin, here pin 8
+#define OLED_PWR_PIN 8
+// connection may depend on the board. Use A5/A4 for most Arduino boards. On ESP8266-based board we use GPI05 and GPI04. Heltec ESP32 has embedded OLED.
+#if defined ARDUINO_Heltec_WIFI_LoRa_32 || defined ARDUINO_WIFI_LoRa_32 || defined HELTEC_LORA
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
+#elif defined ESP8266 || defined ARDUINO_ESP8266_ESP01
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 12, /* data=*/ 14, /* reset=*/ U8X8_PIN_NONE);
+#else
+#if defined OLED_9GND876
+  #ifdef OLED_PWR_PIN
+    #undef OLED_PWR_PIN
+    #define OLED_PWR_PIN 8
+  #endif
+  U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 7, /* data=*/ 6, /* reset=*/ U8X8_PIN_NONE);
+#else
+  U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ A5, /* data=*/ A4, /* reset=*/ U8X8_PIN_NONE);
+#endif
+#endif
+char oled_msg[20];
+#endif
+
+unsigned long nextTransmissionTime=0L;
+
+char *ftoa(char *a, double f, int precision)
+{
+ long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
+
+ char *ret = a;
+ long heiltal = (long)f;
+ itoa(heiltal, a, 10);
+ while (*a != '\0') a++;
+ *a++ = '.';
+ long desimal = abs((long)((f - heiltal) * p[precision]));
+ if (desimal < p[precision-1]) {
+  *a++ = '0';
+ }
+ itoa(desimal, a, 10);
+ return ret;
+}
+
+/*****************************
+ _____      _
+/  ___|    | |
+\ `--.  ___| |_ _   _ _ __
+ `--. \/ _ \ __| | | | '_ \
+/\__/ /  __/ |_| |_| | |_) |
+\____/ \___|\__|\__,_| .__/
+                     | |
+                     |_|
+******************************/
+
+void setup()
+{
+  // initialization of the temperature sensor
+  sensor_Init();
+
+  delay(3000);
+  // Open serial communications and wait for port to open:
+#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0
+  SerialUSB.begin(38400);
+#else
+  Serial.begin(115200);
+#endif
+
+  // Print a start message
+  PRINT_CSTSTR("Simple LoRa sensor demo\n");
+
+#ifdef ARDUINO_AVR_PRO
+  PRINT_CSTSTR("Arduino Pro Mini detected\n");
+#endif
+#ifdef ARDUINO_AVR_NANO
+  PRINT_CSTSTR("Arduino Nano detected\n");
+#endif
+#ifdef ARDUINO_AVR_MINI
+  PRINT_CSTSTR("Arduino Mini/Nexus detected\n");
+#endif
+#ifdef ARDUINO_AVR_MEGA2560
+  PRINT_CSTSTR("Arduino Mega2560 detected\n");
+#endif
+#ifdef ARDUINO_SAM_DUE
+  PRINT_CSTSTR("Arduino Due detected\n");
+#endif
+#ifdef __MK66FX1M0__
+  PRINT_CSTSTR("Teensy36 MK66FX1M0 detected\n");
+#endif
+#ifdef __MK64FX512__
+  PRINT_CSTSTR("Teensy35 MK64FX512 detected\n");
+#endif
+#ifdef __MK20DX256__
+  PRINT_CSTSTR("Teensy31/32 MK20DX256 detected\n");
+#endif
+#ifdef __MKL26Z64__
+  PRINT_CSTSTR("TeensyLC MKL26Z64 detected\n");
+#endif
+#if defined ARDUINO_SAMD_ZERO && not defined ARDUINO_SAMD_FEATHER_M0
+  PRINT_CSTSTR("Arduino M0/Zero detected\n");
+#endif
+#ifdef ARDUINO_AVR_FEATHER32U4
+  PRINT_CSTSTR("Adafruit Feather32U4 detected\n");
+#endif
+#ifdef ARDUINO_SAMD_FEATHER_M0
+  PRINT_CSTSTR("Adafruit FeatherM0 detected\n");
+#endif
+#if defined ARDUINO_ESP8266_ESP01 || defined ARDUINO_ESP8266_NODEMCU || defined ESP8266
+  PRINT_CSTSTR("Expressif ESP8266 detected\n");
+  //uncomment if you want to disable the WiFi, this will reset your board
+  //but maybe it is preferable to use the WiFi.mode(WIFI_OFF), see above
+  //ESP.deepSleep(1, WAKE_RF_DISABLED);
+#endif
+#if defined ARDUINO_Heltec_WIFI_LoRa_32 || defined ARDUINO_WIFI_LoRa_32  || defined HELTEC_LORA
+  PRINT_CSTSTR("Heltec WiFi LoRa 32 detected\n");
+#endif
+
+// See http://www.nongnu.org/avr-libc/user-manual/using_tools.html
+// for the list of define from the AVR compiler
+
+#ifdef __AVR_ATmega328P__
+  PRINT_CSTSTR("ATmega328P detected\n");
+#endif
+#ifdef __AVR_ATmega32U4__
+  PRINT_CSTSTR("ATmega32U4 detected\n");
+#endif
+#ifdef __AVR_ATmega2560__
+  PRINT_CSTSTR("ATmega2560 detected\n");
+#endif
+#ifdef __SAMD21G18A__
+  PRINT_CSTSTR("SAMD21G18A ARM Cortex-M0+ detected\n");
+#endif
+#ifdef __SAM3X8E__
+  PRINT_CSTSTR("SAM3X8E ARM Cortex-M3 detected\n");
+#endif
+#ifdef ESP32
+  PRINT_CSTSTR("ESP32 detected\n");
+#endif
+
+  //start SPI bus communication
+  SPI.begin();
+
+  //setup hardware pins used by device, then check if device is found
+#ifdef SX126X
+  if (LT.begin(NSS, NRESET, RFBUSY, DIO1, DIO2, DIO3, RX_EN, TX_EN, SW, LORA_DEVICE))
+#endif
+
+#ifdef SX127X
+  if (LT.begin(NSS, NRESET, DIO0, DIO1, DIO2, LORA_DEVICE))
+#endif
+
+#ifdef SX128X
+  if (LT.begin(NSS, NRESET, RFBUSY, DIO1, DIO2, DIO3, RX_EN, TX_EN, LORA_DEVICE))
+#endif
+  {
+    PRINT_CSTSTR("LoRa Device found\n");
+    delay(1000);
+  }
+  else
+  {
+    PRINT_CSTSTR("No device responding\n");
+    while (1){ }
+  }
+
+/*******************************************************************************************************
+  Based from SX12XX example - Stuart Robinson
+*******************************************************************************************************/
+
+  //The function call list below shows the complete setup for the LoRa device using the
+  // information defined in the Settings.h file.
+  //The 'Setup LoRa device' list below can be replaced with a single function call;
+  //LT.setupLoRa(Frequency, Offset, SpreadingFactor, Bandwidth, CodeRate, Optimisation);
+
+  //***************************************************************************************************
+  //Setup LoRa device
+  //***************************************************************************************************
+  //got to standby mode to configure device
+  LT.setMode(MODE_STDBY_RC);
+#ifdef SX126X
+  LT.setRegulatorMode(USE_DCDC);
+  LT.setPaConfig(0x04, PAAUTO, LORA_DEVICE);
+  LT.setDIO3AsTCXOCtrl(TCXO_CTRL_3_3V);
+  LT.calibrateDevice(ALLDevices);                //is required after setting TCXO
+  LT.calibrateImage(DEFAULT_CHANNEL);
+  LT.setDIO2AsRfSwitchCtrl();
+#endif
+#ifdef SX128X
+  LT.setRegulatorMode(USE_LDO);
+#endif
+  //set for LoRa transmissions
+  LT.setPacketType(PACKET_TYPE_LORA);
+  //set the operating frequency
+#ifdef MY_FREQUENCY
+  LT.setRfFrequency(MY_FREQUENCY, Offset);
+#else
+  LT.setRfFrequency(DEFAULT_CHANNEL, Offset);
+#endif
+//run calibration after setting frequency
+#ifdef SX127X
+  LT.calibrateImage(0);
+#endif
+  //set LoRa modem parameters
+#if defined SX126X || defined SX127X
+  LT.setModulationParams(SpreadingFactor, Bandwidth, CodeRate, Optimisation);
+#endif
+#ifdef SX128X
+  LT.setModulationParams(SpreadingFactor, Bandwidth, CodeRate);
+#endif
+  //where in the SX buffer packets start, TX and RX
+  LT.setBufferBaseAddress(0x00, 0x00);
+  //set packet parameters
+#if defined SX126X || defined SX127X
+  LT.setPacketParams(8, LORA_PACKET_VARIABLE_LENGTH, 255, LORA_CRC_ON, LORA_IQ_NORMAL);
+#endif
+#ifdef SX128X
+  LT.setPacketParams(12, LORA_PACKET_VARIABLE_LENGTH, 255, LORA_CRC_ON, LORA_IQ_NORMAL, 0, 0);
+#endif
+  //syncword, LORA_MAC_PRIVATE_SYNCWORD = 0x12, or LORA_MAC_PUBLIC_SYNCWORD = 0x34
+#if defined SX126X || defined SX127X
+#ifdef PUBLIC_SYNCWORD
+  LT.setSyncWord(LORA_MAC_PUBLIC_SYNCWORD);
+#else
+  LT.setSyncWord(LORA_MAC_PRIVATE_SYNCWORD);
+#endif
+  //set for highest sensitivity at expense of slightly higher LNA current
+  LT.setHighSensitivity();  //set for maximum gain
+#endif
+#ifdef SX126X
+  //set for IRQ on TX done and timeout on DIO1
+  LT.setDioIrqParams(IRQ_RADIO_ALL, (IRQ_TX_DONE + IRQ_RX_TX_TIMEOUT), 0, 0);
+#endif
+#ifdef SX127X
+  //set for IRQ on TX done
+  LT.setDioIrqParams(IRQ_RADIO_ALL, IRQ_TX_DONE, 0, 0);
+  LT.setPA_BOOST(PA_BOOST);
+#endif
+#ifdef SX128X
+  LT.setDioIrqParams(IRQ_RADIO_ALL, (IRQ_TX_DONE + IRQ_RX_TX_TIMEOUT), 0, 0);
+#endif
+
+  //***************************************************************************************************
+
+  PRINTLN;
+  //reads and prints the configured LoRa settings, useful check
+  LT.printModemSettings();
+  PRINTLN;
+  //reads and prints the configured operating settings, useful check
+  LT.printOperatingSettings();
+  PRINTLN;
+  PRINTLN;
+#if defined SX126X || defined SX127X
+  //print contents of device registers, normally 0x00 to 0x4F
+  LT.printRegisters(0x00, 0x4F);
+#endif
+#ifdef SX128X
+  //print contents of device registers, normally 0x900 to 0x9FF
+  LT.printRegisters(0x900, 0x9FF);
+#endif
+
+/*******************************************************************************************************
+  End from SX12XX example - Stuart Robinson
+*******************************************************************************************************/
+
+#ifdef OLED
+#ifdef OLED_PWR_PIN
+  pinMode(OLED_PWR_PIN, OUTPUT);
+  digitalWrite(OLED_PWR_PIN, HIGH);
+#endif
+  u8x8.begin();
+  //u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.setFont(u8x8_font_pxplustandynewtv_r);
+  u8x8.drawString(0, 0, "Simple TempDemo");
+  sprintf(oled_msg,"SF%dBW%d", LT.getLoRaSF(), LT.returnBandwidth()/1000);
+  u8x8.drawString(0, 1, oled_msg);
+#endif
+
+  PRINT_CSTSTR("Setting Power: ");
+  PRINTLN_VALUE("%d", MAX_DBM);
+
+  LT.setDevAddr(node_addr);
+  PRINT_CSTSTR("node addr: ");
+  PRINT_VALUE("%d", node_addr);
+  PRINTLN;
+
+#ifdef SX126X
+  PRINT_CSTSTR("SX126X");
+#endif
+#ifdef SX127X
+  PRINT_CSTSTR("SX127X");
+#endif
+#ifdef SX128X
+  PRINT_CSTSTR("SX128X");
+#endif
+
+  // Print a success message
+  PRINT_CSTSTR(" successfully configured\n");
+
+  delay(500);
+}
+
+/*****************************
+ _
+| |
+| |     ___   ___  _ __
+| |    / _ \ / _ \| '_ \
+| |___| (_) | (_) | |_) |
+\_____/\___/ \___/| .__/
+                  | |
+                  |_|
+*****************************/
+
+void loop(void)
+{
+  if (millis() > nextTransmissionTime) {
+
+      double sensor_value = sensor_getValue();
+
+      // for testing, uncomment if you just want to test, without a real temp sensor plugged
+      sensor_value = 20.5;
+
+      PRINT_CSTSTR("Sensor value is ");
+      PRINT_VALUE("%f", sensor_value);
+      PRINTLN;
+
+      uint8_t r_size;
+      char float_str[20];
+
+      //convert the floating value into a string
+      ftoa(float_str,sensor_value,2);
+
+      r_size=sprintf((char*)message, "\\!%s/%s", nomenclature_str, float_str);
+
+#ifdef OLED
+      u8x8.clearLine(2);
+      u8x8.clearLine(3);
+      u8x8.clearLine(4);
+      u8x8.clearLine(5);
+      u8x8.clearLine(6);
+      u8x8.drawString(0, 3, "Sending");
+      u8x8.drawString(0, 4, (char*)message);
+#endif
+
+      PRINT_CSTSTR("Sending ");
+      PRINT_STR("%s",(char*)(message));
+      PRINTLN;
+
+      PRINT_CSTSTR("Real payload size is ");
+      PRINT_VALUE("%d", r_size);
+      PRINTLN;
+
+      LT.printASCIIPacket(message, r_size);
+      PRINTLN;
+
+      unsigned long startSend=millis();
+      unsigned long endSend;
+
+      LT.CarrierSense();
+
+      //will return packet length sent if OK, otherwise 0 if transmit error
+      if (LT.transmitAddressed(message, r_size, PKT_TYPE_DATA, DEFAULT_DEST_ADDR, LT.readDevAddr(), 10000, MAX_DBM, WAIT_TX))
+      {
+        endSend = millis();
+        TXPacketCount++;
+        uint16_t localCRC = LT.CRCCCITT(message, r_size, 0xFFFF);
+        PRINT_CSTSTR("CRC,");
+        PRINT_HEX("%d", localCRC);
+      }
+      else
+      {
+        endSend=millis();
+        //if here there was an error transmitting packet
+        uint16_t IRQStatus;
+        IRQStatus = LT.readIrqStatus();
+        PRINT_CSTSTR("SendError,");
+        PRINT_CSTSTR(",IRQreg,");
+        PRINT_HEX("%d", IRQStatus);
+        LT.printIrqStatus();
+      }
+
+      PRINTLN;
+      PRINT_CSTSTR("LoRa pkt size ");
+      PRINT_VALUE("%d", r_size);
+      PRINTLN;
+
+      PRINT_CSTSTR("LoRa pkt seq ");
+      PRINT_VALUE("%d", LT.readTXSeqNo()-1);
+      PRINTLN;
+
+      PRINT_CSTSTR("LoRa Sent in ");
+      PRINT_VALUE("%ld", endSend-startSend);
+      PRINTLN;
+
+#ifdef OLED
+      sprintf(oled_msg, "Sending done");
+      u8x8.drawString(0, 5, oled_msg);
+      u8x8.drawString(0, 6, "Waiting...");
+#endif
+
+      // 1 minute by default
+      nextTransmissionTime=millis()+60000;
+  }
+}
